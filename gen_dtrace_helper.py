21111ede21ccb6d868d199c961f44bef158309fb
Hirokazu MINAMI <hrkzmnm@gmail.com>
2019-11-08 01:19:30 +0900

    refacror

--- gen_dtrace_helper.py
+++ gen_dtrace_helper.py
@@ -8,19 +8,19 @@ from elftools.dwarf.die import DIE
 ENCODING = 'utf-8'
 
 class TypeDG:
-    TAGS_for_types = (
-        "DW_TAG_array_type",
-        "DW_TAG_enumeration_type",
-        "DW_TAG_structure_type",
-        "DW_TAG_typedef",
-        "DW_TAG_union_type",
-        "DW_TAG_subprogram",
-    )
-    TAGS_for_qualifiers = (
+    TAGS_for_types = {
+        "DW_TAG_array_type": None,
+        "DW_TAG_enumeration_type": "enum",
+        "DW_TAG_structure_type": "struct",
+        "DW_TAG_typedef": "typedef",
+        "DW_TAG_union_type": "union",
+        "DW_TAG_subprogram": None,
+    }
+    TAGS_for_qualifiers = {
         "DW_TAG_const_type",
         "DW_TAG_volatile_type",
         "DW_TAG_restrict_type",
-    )
+    }
 
     def __init__(self,
                  CU: elftools.dwarf.compileunit.CompileUnit,
@@ -33,23 +33,20 @@ class TypeDG:
         # attr['DW_AT_decl_file'] -> name
         self.filedesc = dict( (le.dir_index, le.name.decode(ENCODING))
                               for le in line_program['file_entry'])
-        print(self.filedesc, line_program['file_entry'])
+
         named_types = {}
         def walk(die, names, depth: int = 0):
-            name = self._get_attr__name(die)
-            # print(depth, die.tag, name)
+            name = self._get_die_name(die)
             if (die.tag in self.TAGS_for_types) and name:
                 names.setdefault(name, set()).add(die)
-            # offset -> die
             yield ((die.offset - self.cu_offset), die)
             for child in die.iter_children():
                 yield from walk(child, names, depth+1)
 
         self.offset_to_die = dict(walk(top, named_types))
         self.named_types = named_types
-
         
-    def _get_attr__type_die(self, die :DIE) -> Optional[DIE]:
+    def _get_type_die(self, die :DIE) -> Optional[DIE]:
         if 'DW_AT_type' in die.attributes:
             value = die.attributes["DW_AT_type"].value
             return self.offset_to_die.get(value, None)
@@ -63,9 +60,9 @@ class TypeDG:
             if 0 in self.filedesc:
                 dir_index -= 1
             srcfile = self.filedesc.get(dir_index,
-                                        "+nowhere{}_".format(dir_index))
+                                        "_nowhere{}_".format(dir_index))
         else:
-            srcfile = "+nowhere_"
+            srcfile = "_nowhere_"
         loc_line = die.attributes.get('DW_AT_decl_line', None)
         if loc_line:
             srcline = ":{}".format(loc_line.value)
@@ -73,11 +70,17 @@ class TypeDG:
             srcline = ""
         return (srcfile + srcline)
 
-    def _get_attr__name(self, die :DIE, gensym: bool = False) -> Optional[str]:
+    def _get_stem(self, die):
+        stem = self.TAGS_for_types.get(die.tag, None)
+        if stem is None:
+            raise Exception("no stem is known for", die)
+        return stem
+
+    def _get_die_name(self, die :DIE, gensym: bool = False) -> Optional[str]:
         if 'DW_AT_name' in die.attributes:
             return die.attributes["DW_AT_name"].value.decode(ENCODING)
         if gensym:
-            stem = die.tag.split("_")[2]
+            stem = self._get_stem(die)
             return ("anon_" + stem
                     + "_{:x}_{:x}".format(self.cu_offset, die.offset))
         return None
@@ -99,64 +102,53 @@ class TypeDG:
         
         elif die.tag == "DW_TAG_base_type":
             if name:
-                return (self._get_attr__name(die) + " " + name)
-            return (self._get_attr__name(die))
-
-        elif die.tag == "DW_TAG_const_type":
-            return ("const "
-                    + self.gen_decl(self._get_attr__type_die(die), shown, name))
-
-        elif die.tag == "DW_TAG_volatile_type":
-            return ("volatile "
-                    + self.gen_decl(self._get_attr__type_die(die), shown, name))
+                return (self._get_die_name(die) + " " + name)
+            return (self._get_die_name(die))
 
         elif die.tag == "DW_TAG_pointer_type":
-            return (self.gen_decl(self._get_attr__type_die(die), shown,
+            return (self.gen_decl(self._get_type_die(die), shown,
                                   "*" + (name if name else "")))
         
-        elif die.tag == "DW_TAG_typedef":
-            dep = self._get_attr__type_die(die)
-            if shown.get(dep, None) != "defined":
-                self.track(dep, shown)
-            return (self._get_attr__name(die)
-                    + " " + (name if name else ""))
-
         elif die.tag == "DW_TAG_subroutine_type":
-            rettype = self._get_attr__type_die(die)
+            rettype = self._get_type_die(die)
             args = []
             for child in die.iter_children():
                 if child.tag != "DW_TAG_formal_parameter":
                     continue
-                args.append(self.gen_decl(self._get_attr__type_die(child), shown, None))
+                args.append(self.gen_decl(self._get_type_die(child), shown, None))
             if not args:
                 args = [self.gen_decl(None, shown, None)]
             return (self.gen_decl(rettype, shown, None)
                     + " (" + name + ")(" + (", ".join(args)) + ")")
 
-        elif die.tag == "DW_TAG_structure_type":
-            return ("struct "
-                    + self._get_attr__name(die)
-                    + " " + (name if name else ""))
+        elif die.tag == "DW_TAG_const_type":
+            return ("const "
+                    + self.gen_decl(self._get_type_die(die), shown, name))
 
-        elif die.tag == "DW_TAG_union_type":
-            return ("union "
-                    + self._get_attr__name(die, True)
-                    + " " + (name if name else ""))
+        elif die.tag == "DW_TAG_volatile_type":
+            return ("volatile "
+                    + self.gen_decl(self._get_type_die(die), shown, name))
 
         elif die.tag == "DW_TAG_array_type":
-            elemtype = self._get_attr__type_die(die)
+            elemtype = self._get_type_die(die)
             count = "[]"
             for child in die.iter_children():
-                if child.tag == "DW_TAG_subrange_type":
-                    if not "DW_AT_count" in child.attributes:
-                        continue
-                    count = "[{}]".format(child.attributes["DW_AT_count"].value)
+                if child.tag != "DW_TAG_subrange_type":
+                    continue
+                if not "DW_AT_count" in child.attributes:
+                    continue
+                count = "[{}]".format(child.attributes["DW_AT_count"].value)
             return (self.gen_decl(elemtype, shown, None) + " " + name + count)
 
-        elif die.tag == "DW_TAG_enumeration_type":
-            return ("enum "
-                    + self._get_attr__name(die)
+        elif die.tag == "DW_TAG_typedef":
+            return (self._get_die_name(die)
+                    + " " + (name if name else ""))
+
+        elif not (self.TAGS_for_types.get(die.tag, None) is None):
+            return (self._get_stem(die) + " "
+                    + self._get_die_name(die, True)
                     + ((" " + name) if name else ""))
+            
 
         raise Exception("cannot generate decl. for ", die)
         return die.tag
@@ -177,78 +169,55 @@ class TypeDG:
         if die.tag == "DW_TAG_base_type":
             pass
 
-        elif die.tag == "DW_TAG_subroutine_type":
-            self.track(self._get_attr__type_die(die), shown, depth)
+        elif die.tag in ("DW_TAG_subprogram",
+                         "DW_TAG_subroutine_type"):
+            self.track(self._get_type_die(die), shown, depth)
             for child in die.iter_children():
                 if child.tag != "DW_TAG_formal_parameter":
                     continue
-                self.track(self._get_attr__type_die(child), shown, depth)
+                self.track(self._get_type_die(child), shown, depth)
             
         elif die.tag == "DW_TAG_pointer_type":
-            self.track(self._get_attr__type_die(die), shown, depth, True)
+            self.track(self._get_type_die(die), shown, depth, True)
 
         elif die.tag in self.TAGS_for_qualifiers:
-            self.track(self._get_attr__type_die(die), shown, depth)
+            self.track(self._get_type_die(die), shown, depth)
 
         elif die.tag == "DW_TAG_typedef":
-            dep = self._get_attr__type_die(die)
+            dep = self._get_type_die(die)
             self.track(dep, shown, depth)
             print("\n/* @", self._get_attr__srcloc(die), "*/")
-            print("typedef " + self.gen_decl(dep, shown, self._get_attr__name(die)) + ";")
+            print("typedef " + self.gen_decl(dep, shown, self._get_die_name(die)) + ";")
             
-        elif die.tag == "DW_TAG_structure_type":
-            name = self._get_attr__name(die);
+        elif die.tag in ("DW_TAG_structure_type",
+                         "DW_TAG_union_type"):
             if maybe_incomplete:
-                print("struct " + name + ";")
+                print(self.gen_decl(die, shown, None) + ";")
                 decl_only = True
+            elif "DW_AT_declaration" in die.attributes:
+                return
             else:
                 members = []
                 for child in die.iter_children():
                     if child.tag != "DW_TAG_member":
                         continue
-                    mname = self._get_attr__name(child);
-                    mtype = self._get_attr__type_die(child)
+                    mname = self._get_die_name(child);
+                    mtype = self._get_type_die(child)
                     moff = child.attributes['DW_AT_data_member_location'].value
                     self.track(mtype, shown, depth)
                     members.append("\t" + self.gen_decl(mtype, shown, mname)
                                    + ";\t/* +0x{:x} */".format(moff));
+                print("\n/* @", self._get_attr__srcloc(die), "*/")
+                print(self.gen_decl(die, shown, None)
+                      + " {"
+                      + "\t/* size=0x{:x} */".format(die.attributes["DW_AT_byte_size"].value))
                 if members:
-                    print("\n/* @", self._get_attr__srcloc(die), "*/")
-                    print("struct " + name + " {")
                     for line in members:
                         print(line)
-                    print("};")
-                else:
-                    return # empty struct?
+                print("};")
 
-        elif die.tag == "DW_TAG_union_type":
-            name = self._get_attr__name(die, True);
-            if maybe_incomplete:
-                print("union " + name + ";")
-                decl_only = True
-            else:
-                members = []
-                for child in die.iter_children():
-                    if child.tag != "DW_TAG_member":
-                        continue
-                    mname = self._get_attr__name(child);
-                    mtype = self._get_attr__type_die(child)
-                    moff = child.attributes['DW_AT_data_member_location'].value
-                    #print(name, "has member:", child, "\n\t-> mtype:", mtype)
-                    self.track(mtype, shown, depth)
-                    members.append("\t" + self.gen_decl(mtype, shown, mname)
-                                   + "; /* +0x{:x} */".format(moff));
-                if members:
-                    print("\n/* @", self._get_attr__srcloc(die), "*/")
-                    print("union " + name + " {")
-                    for line in members:
-                        print(line)
-                    print("};")
-                else:
-                    return # empty union?
-                
         elif die.tag == "DW_TAG_array_type":
-            elemtype = self._get_attr__type_die(die)
+            elemtype = self._get_type_die(die)
             self.track(elemtype, shown, depth)
 
         elif die.tag == "DW_TAG_enumeration_type":
@@ -258,18 +227,16 @@ class TypeDG:
                     continue
                 ctval = child.attributes["DW_AT_const_value"]
                 members.append( (",\t" if members else "\t")
-                                + self._get_attr__name(child)
+                                + self._get_die_name(child)
                                 + (" = {}".format(ctval.value) if ctval else ""))
             print(self.gen_decl(die, shown, None) + " {")
             for line in members:
                 print(line)
             print("};")
 
-        elif die.tag == "DW_TAG_subprogram":
-            pass
-
         else:
             raise Exception("unhandled DIE", die)
+
         if decl_only:
             if shown.get(die, None) != "defined":
                 shown[die] = "declared"
