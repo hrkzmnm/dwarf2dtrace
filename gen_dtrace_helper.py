cf6e6d302cb387f1158ea7e7f988f3d76e76b604
Hirokazu MINAMI <hrkzmnm@gmail.com>
2019-11-27 21:41:28 +0900

    cleanup

--- gen_dtrace_helper.py
+++ gen_dtrace_helper.py
@@ -5,9 +5,6 @@ import re
 import sys
 import dataclasses
 
-import elftools.elf.elffile
-from elftools.dwarf.die import DIE
-
 ENCODING = 'utf-8'
 class ParseError(Exception):
     pass
@@ -22,20 +19,23 @@ class Node:
     is_decl: bool
     decl_file: str
     decl_line: int
+    data_member_location: int # for members
     byte_size: int
     bit_size: int # bitfields
+    bit_offset: int # bitfields
     deps: Iterable[int] # for composite types
     quantity: int # for arrays
-    data_member_location: int # for members
     def src_location(self) -> str:
-        return f"{self.decl_file}:{self.decl_line}"
+        if not self.decl_line is None:
+            return f"{self.decl_file}:{self.decl_line}"
+        return f"{self.decl_file}"
 
 class TypeDG:
     TAGS_for_types = {
         "DW_TAG_array_type": None,
         "DW_TAG_enumeration_type": "enum",
         "DW_TAG_structure_type": "struct",
-        "DW_TAG_class_type": "/*<class>*/struct", # interpret as a struct
+        "DW_TAG_class_type": "/*<class>*/struct",
         "DW_TAG_typedef": "typedef",
         "DW_TAG_union_type": "union",
         "DW_TAG_subprogram": None,
@@ -54,173 +54,158 @@ class TypeDG:
         return True
 
     def __init__(self):
-        self.symbols = {}
         self.offset_to_node = {}
 
-    def summarize(self, die: DIE):
-        name = None
-        if 'DW_AT_name' in die.attributes:
-            name = die.attributes["DW_AT_name"].value.decode(ENCODING)
-            if not self.is_valid_name(name):
-                name = None
-        nickname = name
-        if nickname is None:
-            keyword = self.TAGS_for_types.get(die.tag, None)
-            if keyword:
-                nickname = f"anon_{keyword}__GOFF0x{die.offset:x}"
-        def get_type_goff(die):
-            at_type = die.attributes.get('DW_AT_type', None)
-            if not at_type:
-                return -1
-            if at_type.form == "DW_FORM_ref_addr":
-                # global offset (needs relocation?)
-                return at_type.value
-            if at_type.form == "DW_FORM_ref_sig8":
-                # 8-byte type signature
-                raise ParseError("cannot handle {at_type.form} yet")
-            if at_type.form in {"DW_FORM_ref_sup4", "DW_FORM_ref_sup8"}:
-                # supplementary obj file?
-                raise ParseError("cannot handle {at_type.form} yet")
-            # for _ref[1248] or _ref_udata, CU-local offset
-            return at_type.value + die.cu.cu_offset
-        def get_decl_file(die):
-            if not die.tag in self.TAGS_for_types:
-                return "_omitted_"
-            decl_file = die.attributes.get('DW_AT_decl_file', None)
-            if decl_file:
-                # note: no need to -1 for DWARFv5?
-                fileno = decl_file.value - 1
-                line_program = die.cu.dwarfinfo.line_program_for_CU(die.cu)
-                return line_program['file_entry'][fileno].name.decode(ENCODING)
-            else:
-                return "_nowhere_"
-        def get_decl_line(die):
-            decl_line = die.attributes.get('DW_AT_decl_line', None)
-            return decl_line.value if decl_line else -1
-        def get_byte_size(die):
-            byte_size = die.attributes.get('DW_AT_byte_size')
-            return byte_size.value if byte_size else -1
-        def get_bit_size(die):
-            bit_size = die.attributes.get('DW_AT_bit_size')
-            return bit_size.value if bit_size else None
-        deps = None
-        if die.tag in ("DW_TAG_structure_type",
-                       "DW_TAG_class_type",
-                       "DW_TAG_union_type"):
-            deps = [child.offset for child in die.iter_children()
-                    if child.tag == "DW_TAG_member"]
-        elif die.tag in ("DW_TAG_subprogram",
-                         "DW_TAG_subroutine_type"):
-            deps = [child.offset for child in die.iter_children()
-                    if child.tag == "DW_TAG_formal_parameter"]
-        elif die.tag == "DW_TAG_enumeration_type":
-            deps = [child.offset for child in die.iter_children()
-                    if child.tag == "DW_TAG_enumerator"]
-        def get_quantity(die):
-            if die.tag == "DW_TAG_array_type":
-                for child in die.iter_children():
-                    if child.tag != "DW_TAG_subrange_type":
-                        continue
-                    if not "DW_AT_count" in child.attributes:
-                        continue
-                    return child.attributes['DW_AT_count'].value
-            if die.tag == "DW_TAG_enumerator":
-                ctval = die.attributes["DW_AT_const_value"]
-                if ctval:
-                    return ctval.value
-            return None
-        data_member_location = None
-        if die.tag == "DW_TAG_member":
-            if "DW_AT_data_member_location" in die.attributes:
-                data_member_location = die.attributes["DW_AT_data_member_location"].value
-            elif  "DW_AT_bit_offset" in die.attributes:
-                data_member_location = die.attributes["DW_AT_bit_offset"].value
-        return Node(
-            tag = sys.intern(die.tag),
-            offset = die.offset,
-            name = name,
-            nickname = nickname,
-            type_goff = get_type_goff(die),
-            is_decl = ("DW_AT_declaration" in die.attributes),
-            decl_file = sys.intern(get_decl_file(die)),
-            decl_line = get_decl_line(die),
-            byte_size = get_byte_size(die),
-            bit_size = get_bit_size(die),
-            deps = deps,
-            quantity = get_quantity(die),
-            data_member_location = data_member_location,
-        )
-    def parse_cu(self, CU: elftools.dwarf.compileunit.CompileUnit):
+    def parse_file(self, f): 
         from elftools.dwarf import constants
-        top = CU.get_top_DIE()
-        if not top.attributes['DW_AT_language'].value in {
-                constants.DW_LANG_C,
-                constants.DW_LANG_C89,
-                constants.DW_LANG_C99,
-                constants.DW_LANG_C11 if 'DW_LANG_C11' in dir(constants) else 0x1d,
-                # constants.DW_LANG_C_plus_plus,
-                # constants.DW_LANG_C_plus_plus_03,
-                # constants.DW_LANG_C_plus_plus_11,
-                # constants.DW_LANG_C_plus_plus_14,
-        }:
-            return
-        if self.VERBOSE > 0:
-            print(f"\n/** CU 0x{CU.cu_offset:x} '{top.get_full_path()}' **/")
-        def register_die(die: DIE):
+        import elftools.elf.elffile
+        efile = elftools.elf.elffile.ELFFile(f)
+        dwinfo = efile.get_dwarf_info(relocate_dwarf_sections=False)
+        def register_die(die):
+            def build_node(die):
+                def get_die_attr(die, attrname, default = None):
+                    attr = die.attributes.get(attrname, None)
+                    return default if (attr is None) else attr.value
+                name = None
+                if 'DW_AT_name' in die.attributes:
+                    name = die.attributes["DW_AT_name"].value.decode(ENCODING)
+                    if not self.is_valid_name(name):
+                        name = None
+                def gen_nickname(die, name):
+                    if name:
+                        return name
+                    keyword = self.TAGS_for_types.get(die.tag, None)
+                    if not keyword is None:
+                        return f"anon_{keyword}__GOFF0x{die.offset:x}"
+                    return None
+                def get_type_goff(die):
+                    at_type = die.attributes.get('DW_AT_type', None)
+                    if not at_type:
+                        return None
+                    if at_type.form == "DW_FORM_ref_addr":
+                        # global offset
+                        return at_type.value
+                    if at_type.form == "DW_FORM_ref_sig8":
+                        # 8-byte type signature
+                        raise ParseError("cannot handle {at_type.form} yet")
+                    if at_type.form in {"DW_FORM_ref_sup4", "DW_FORM_ref_sup8"}:
+                        # supplementary obj file?
+                        raise ParseError("cannot handle {at_type.form} yet")
+                    # for _ref[1248] or _ref_udata, CU-local offset
+                    return at_type.value + die.cu.cu_offset
+                def get_decl_file(die):
+                    if not die.tag in self.TAGS_for_types:
+                        return "_omitted_"
+                    decl_file = get_die_attr(die, 'DW_AT_decl_file')
+                    if decl_file:
+                        # no need to -1 for DWARFv5?
+                        fileno = decl_file - 1
+                        line_program = die.cu.dwarfinfo.line_program_for_CU(die.cu)
+                        return line_program['file_entry'][fileno].name.decode(ENCODING)
+                    else:
+                        return "_nowhere_"
+                def get_deps(die):
+                    def gather(die, tag):
+                        return tuple(child.offset for child in die.iter_children()
+                                     if child.tag == tag)
+                    if die.tag in ("DW_TAG_structure_type",
+                                   "DW_TAG_class_type",
+                                   "DW_TAG_union_type"):
+                        return gather(die, "DW_TAG_member")
+                    if die.tag in ("DW_TAG_subprogram",
+                                   "DW_TAG_subroutine_type"):
+                        return tuple(child.offset for child in die.iter_children()
+                                     if child.tag == "DW_TAG_formal_parameter")
+                    if die.tag == "DW_TAG_enumeration_type":
+                        return tuple(child.offset for child in die.iter_children()
+                                     if child.tag == "DW_TAG_enumerator")
+                    return None
+                def get_quantity(die):
+                    if die.tag == "DW_TAG_array_type":
+                        for child in die.iter_children():
+                            if child.tag != "DW_TAG_subrange_type":
+                                continue
+                            return get_die_attr(child, "DW_AT_count")
+                    if die.tag == "DW_TAG_enumerator":
+                        return get_die_attr(die, "DW_AT_const_value") 
+                    return None
+                def get_memory_location(die):
+                    data_member_location = None
+                    if die.tag == "DW_TAG_member":
+                        if "DW_AT_data_member_location" in die.attributes:
+                            return die.attributes["DW_AT_data_member_location"].value
+                        elif  "DW_AT_bit_offset" in die.attributes:
+                            return die.attributes["DW_AT_bit_offset"].value
+                    return None
+                return Node(
+                    tag = sys.intern(die.tag),
+                    offset = die.offset,
+                    name = name,
+                    nickname = gen_nickname(die, name),
+                    type_goff = get_type_goff(die),
+                    is_decl = ("DW_AT_declaration" in die.attributes),
+                    decl_file = sys.intern(get_decl_file(die)),
+                    decl_line = get_die_attr(die, 'DW_AT_decl_line'),
+                    byte_size =  get_die_attr(die, 'DW_AT_byte_size'),
+                    data_member_location = get_memory_location(die),
+                    bit_size = get_die_attr(die, 'DW_AT_bit_size'),
+                    bit_offset = get_die_attr(die, 'DW_AT_bit_offset'),
+                    deps = get_deps(die),
+                    quantity = get_quantity(die),
+                )
             try:
-                node = self.summarize(die)
+                node = build_node(die)
             except ParseError as e:
                 print(f"/* ignored {die.tag} at {die.offset}: {str(e)} */")
                 return
-            self.offset_to_node[die.offset] = node
-            symolname = None
+            self.offset_to_node[node.offset] = node
             if not (die.tag in self.TAGS_for_types):
                 return
-            if "DW_AT_declaration" in die.attributes:
-                return
-            symolname = node.name
-            if symolname is None:
-                return
-            try:
-                known = self.symbols[symolname]
-            except KeyError as e: 
-                self.symbols[symolname] = node
-                if self.VERBOSE > 0:
-                    print(f"/* got {symolname}"
-                          f" GOFF=0x{die.offset:x}"
-                          f" at {node.src_location()} */")
-            else:
-                if self.VERBOSE > 0:
-                    print(f"/* duplicated {symolname}"
-                          f" GOFF=0x{die.offset:x} at {node.src_location()},"
-                          f" which is known as"
-                          f" GOFF=0x{known.offset:x} at {known.src_location()} */")
+            if self.VERBOSE > 0 and node.name:
+                print(f"/* '{node.name}' is {node.tag}"
+                      f" GOFF0x{node.offset:x},"
+                      f" {node.src_location()} */")
 
         def walk(die, depth: int = 0):
             register_die(die)
             for child in die.iter_children():
                 walk(child, depth+1)
-        walk(top)
+        for CU in dwinfo.iter_CUs():
+            top = CU.get_top_DIE()
+            if self.VERBOSE > 0:
+                print(f"\n/** CU GOFF0x{CU.cu_offset:x} '{top.get_full_path()}' **/")
+            if top.attributes['DW_AT_language'].value in {
+                    constants.DW_LANG_C,
+                    constants.DW_LANG_C89,
+                    constants.DW_LANG_C99,
+                    constants.DW_LANG_C11 if 'DW_LANG_C11' in dir(constants) else 0x1d,
+                    # constants.DW_LANG_C_plus_plus,
+                    # constants.DW_LANG_C_plus_plus_03,
+                    # constants.DW_LANG_C_plus_plus_11,
+                    # constants.DW_LANG_C_plus_plus_14,
+            }:
+                walk(top)
 
     def type_of(self, node :Node) -> Optional[Node]:
         value = node.type_goff
-        if value == -1:
+        if value is None:
             return None
         try:
             return self.offset_to_node[value]
         except KeyError as e:
-            raise ParseError(f"no DIE at offset=0x{value:x}") from e
+            raise ParseError(f"no node for offset=0x{value:x}") from e
 
-    def explain(self,
-                checker: Callable[[Node], bool] = None):
+    def explain(self, checker: Callable[[Node], bool] = None):
         shown = {}
-        for name, node in self.symbols.items():
-            if not checker(node):
+        for goff, node in self.offset_to_node.items():
+            if not node.tag in self.TAGS_for_types:
+                continue
+            if checker and not checker(node):
                 continue
             try:
                 self.track(node, shown, 0)
             except ParseError as e:
-                print(f"/* skipped {node.tag} '{name}'"
+                print(f"/* skipped {node.tag} '{node.nickname}'"
                       f" at {node.src_location()}: {str(e)} */")
 
     def gen_decl(self, node: Optional[Node], name: str = None) -> str:
@@ -257,11 +242,11 @@ class TypeDG:
                     + " (" + name + ")(" + (", ".join(fparams)) + ")")
 
         if node.tag == "DW_TAG_array_type":
-            count = "[]"
+            postfix = "[]"
             if not node.quantity is None:
-                count = f"[{node.quantity}]"
+                postfix = f"[{node.quantity}]"
             return (self.gen_decl(self.type_of(node))
-                    + " " + name + count)
+                    + " " + name + postfix)
 
         if self.TAGS_for_qualifiers.get(node.tag, None):
             if node.tag == "DW_TAG_restrict_type":
@@ -293,11 +278,10 @@ class TypeDG:
               maybe_incomplete: bool = False):
         if node is None:
             return
-        depth = depth + 1
-
         if node.tag == "DW_TAG_base_type":
             return
 
+        depth = depth + 1
         if node.tag == "DW_TAG_pointer_type":
             try:
                 self.track(self.type_of(node), shown, depth, True)
@@ -386,23 +370,26 @@ class TypeDG:
                     raise ParseError(f"failed to get {mname}'s doff")                    
                 mname = child.name
                 if not mname:
-                    mname = f"unnamed{len(members)}__goff_0x{mloc:x}"
+                    mname = f"unnamed{len(members)}__off0x{mloc:x}"
                 try:
                     self.track(mtype, shown, depth)
                 except ParseError as e:
                     raise ParseError(f"failed to track a member"
-                                     f" {mtype.tag} '{mname}' " + str(e))
+                                     f" {mtype.tag} '{mname}' {str(e)}")
                 if not child.bit_size is None:
                     mname += f":{child.bit_size}"
                 members.append(f"\t{self.gen_decl(mtype, mname)};"
                                f"\t/* +0x{mloc:x} */");
             print(f"\n/* @ {node.src_location()} */")
-            print(self.gen_decl(node)+ " {"
-                  f"/* size=0x{node.byte_size:x} */")
+            if node.byte_size is None:
+                print(f"{self.gen_decl(node)} {{")
+            else:
+                print(f"{self.gen_decl(node)} {{"
+                      f"/* size=0x{node.byte_size:x} */")
             if members:
                 for line in members:
                     print(line)
-            elif node.byte_size > 0:
+            elif not node.byte_size is None:
                 print(f"\tchar dummy[0x{node.byte_size:x}];")
             print("};")
             return
@@ -424,19 +411,15 @@ class TypeDG:
             print("};")
             return
 
-        else:
-            raise ParseError("incompatible tag: " + node.tag)
+        raise ParseError("incompatible tag: " + node.tag)
 
 
 if __name__ == '__main__':
     # sys.setrecursionlimit(100)
     path = sys.argv[1]
     with open(path, 'rb') as f:
-        efile = elftools.elf.elffile.ELFFile(f)
-        dwinfo = efile.get_dwarf_info(relocate_dwarf_sections=False)
         dg = TypeDG()
-        for CU in dwinfo.iter_CUs():
-            dg.parse_cu(CU)
+        dg.parse_file(f)
         def everything(node: Node):
             return True
         dg.explain(everything)
